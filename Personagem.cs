596d4c5cff541b6d6c510f1197e75fadfda3b99e
Your Name <renato_inovacao@hotmail.com>
2020-12-01 10:17:08 -0300

    Finalizado

---
+++ Personagem.cs
@@ -0,0 +1,30 @@
+namespace PrimeiroPOO
+{
+    public class Personagem
+    {
+        public string nome;
+
+        public int idade;
+
+        public string armadura;
+
+
+
+        public string Atacar(){
+            return "Ataque de fogo!";
+        }
+
+        public string Defender(){
+            return "Não foi dessa vez!";
+        }
+
+        public string Atacar1(){
+            return "Ataque de raios!";
+        }
+
+        public string Defender1(){
+            return "Ataque neutralizado!";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrimeiroPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            Personagem objetoPersonagem1 = new Personagem();
+
+            objetoPersonagem1.nome = "Renato";
+            objetoPersonagem1.idade = 23;
+            objetoPersonagem1.armadura = "Fulltec Ancient";
+
+            Personagem objetoPersonagem2 = new Personagem();
+
+            objetoPersonagem2.nome = "Tiago";
+            objetoPersonagem2.idade = 20;
+            objetoPersonagem2.armadura = " Relikia full Ancient";
+
+
+            Console.WriteLine($"Personagem 1 = {objetoPersonagem1.nome} - {objetoPersonagem1.Atacar()}");
+            Console.WriteLine($"Personagem 2 = {objetoPersonagem2.nome} - {objetoPersonagem1.Defender()}");
+            
+            Console.WriteLine($"Personagem 1 = {objetoPersonagem1.nome} - {objetoPersonagem1.Atacar1()}");
+            Console.WriteLine($"Personagem 2 = {objetoPersonagem2.nome} - {objetoPersonagem1.Defender1()}");
+
+
+
+        }
+    }
+}
